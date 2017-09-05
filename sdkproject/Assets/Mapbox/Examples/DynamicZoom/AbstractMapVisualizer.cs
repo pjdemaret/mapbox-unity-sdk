69bf7bb76734f1dc8c63df8219eab08e91dba968
BergWerkGIS <wb@BergWerk-GIS.at>
2017-09-05 15:31:20 +0200

    add more changed files

---
+++ sdkproject/Assets/Mapbox/Examples/DynamicZoom/AbstractMapVisualizer.cs
@@ -0,0 +1,147 @@
+﻿namespace Mapbox.Unity.MeshGeneration
+{
+	using System.Collections;
+	using System.Collections.Generic;
+	using UnityEngine;
+	using Mapbox.Map;
+	using Mapbox.Unity.MeshGeneration.Factories;
+	using Mapbox.Unity.MeshGeneration.Data;
+	using System;
+	using Mapbox.Unity.Map;
+	using Mapbox.Platform;
+
+	public abstract class AbstractMapVisualizer : ScriptableObject, IMapVisualizer
+	{
+		public AbstractTileFactory[] Factories { get { return _factories; } }
+
+		[SerializeField]
+		private AbstractTileFactory[] _factories;
+
+		public IMap _map { get; internal set; }
+
+		public Queue<UnityTile> InactiveTiles { get; set; }
+
+
+		public Dictionary<UnwrappedTileId, UnityTile> Tiles { get; set; }
+
+		private ModuleState _state;
+		public ModuleState State
+		{
+			get
+			{
+				return _state;
+			}
+			internal set
+			{
+				if (_state != value)
+				{
+					_state = value;
+					OnMapVisualizerStateChanged(_state);
+				}
+			}
+		}
+
+		public event Action<ModuleState> OnMapVisualizerStateChanged = delegate { };
+
+		/// <summary>
+		/// Initializes the factories by passing the file source down, which's necessary for data (web/file) calls
+		/// </summary>
+		/// <param name="fileSource"></param>
+		public void Initialize(IMap map, IFileSource fileSource)
+		{
+			_map = map;
+			Tiles = new Dictionary<UnwrappedTileId, UnityTile>();
+			InactiveTiles = new Queue<UnityTile>();
+			State = ModuleState.Initialized;
+
+			foreach (var factory in Factories)
+			{
+				factory.Initialize(fileSource);
+				factory.OnFactoryStateChanged += UpdateState;
+			}
+		}
+
+		public void Destroy()
+		{
+			for (int i = 0; i < Factories.Length; i++)
+			{
+				if (Factories[i] != null)
+					Factories[i].OnFactoryStateChanged -= UpdateState;
+			}
+		}
+
+
+		internal void UpdateState(AbstractTileFactory factory)
+		{
+			if (State != ModuleState.Working && factory.State == ModuleState.Working)
+			{
+				State = ModuleState.Working;
+			}
+			else if (State != ModuleState.Finished && factory.State == ModuleState.Finished)
+			{
+				var allFinished = true;
+				for (int i = 0; i < Factories.Length; i++)
+				{
+					if (Factories[i] != null)
+					{
+						allFinished &= Factories[i].State == ModuleState.Finished;
+					}
+				}
+				if (allFinished)
+				{
+					State = ModuleState.Finished;
+				}
+			}
+		}
+
+
+		/// <summary>
+		/// Registers requested tiles to the factories
+		/// </summary>
+		/// <param name="tileId"></param>
+		public virtual UnityTile LoadTile(UnwrappedTileId tileId)
+		{
+			UnityTile unityTile = null;
+
+			if (InactiveTiles.Count > 0)
+			{
+				unityTile = InactiveTiles.Dequeue();
+			}
+
+			if (unityTile == null)
+			{
+				unityTile = new GameObject().AddComponent<UnityTile>();
+				unityTile.transform.SetParent(_map.Root, false);
+			}
+
+			unityTile.Initialize(_map, tileId);
+
+			foreach (var factory in Factories)
+			{
+				factory.Register(unityTile);
+			}
+
+			Tiles.Add(tileId, unityTile);
+
+			return unityTile;
+		}
+
+		public void DisposeTile(UnwrappedTileId tileId)
+		{
+			var unityTile = Tiles[tileId];
+
+			unityTile.Recycle();
+			Tiles.Remove(tileId);
+			InactiveTiles.Enqueue(unityTile);
+
+			foreach (var factory in Factories)
+			{
+				factory.Unregister(unityTile);
+			}
+		}
+
+
+
+
+	}
+}
